54c9f93eafc23501398d60ff5b6d0d75b880a538
wuyaheng <wuyaheng2016@gmail.com>
2020-07-16 23:09:07 -0400

    get all data from database

--- client/src/components/layout/Card/CardItem.js
+++ client/src/components/layout/Card/CardItem.js
@@ -35,6 +35,31 @@ const CardItem = ({ profile }) => {
                         <i className='fas fa-phone' /> {lastname}
                     </li>
                 )}
+                {city && (
+                    <li>
+                        <i className='fas fa-phone' /> {city}
+                    </li>
+                )}
+                {zipcode && (
+                    <li>
+                        <i className='fas fa-phone' /> {zipcode}
+                    </li>
+                )}
+                {skillshare && (
+                    <li>
+                        <i className='fas fa-phone' /> {skillshare}
+                    </li>
+                )}
+                {skillwanted && (
+                    <li>
+                        <i className='fas fa-phone' /> {skillwanted}
+                    </li>
+                )}
+                {bio && (
+                    <li>
+                        <i className='fas fa-phone' /> {bio}
+                    </li>
+                )}
             </ul>
             <p>
                 <button className="btn btn-dark btn-sm" onClick={() => setCurrent(profile)}>Edit</button>
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,7 +1,6 @@
 import React, { useContext,useEffect } from 'react'
 import AuthContext from '../../context/auth/authContext'
 import Card from '../layout/Card/Card'
-// import CardItem from '../layout/Card/CardItem'
 
 const Home = () => {
     const authContext = useContext(AuthContext);
